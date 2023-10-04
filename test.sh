05429cdec4f3db802096de9aac017fed4323e382
Zikin18 <zikin1988@mail.ru>
2023-10-04 23:21:56 +0300

    feat(brain): добавил загадку

--- test.sh
+++ test.sh
@@ -14,3 +14,38 @@ select ans in сложение вычитание умножение делен�
 done
 ans=$(echo "$n1 $op $n2" | bc -l)
 printf "%s %s %s = %s\n\n" "$n1" "$op" "$n2" "$ans"
+
+read -p "Число для головоломки" number
+result=1
+if (( number == 1 || number == 0 )); then
+    echo
+    echo number: $number
+    echo
+    else
+    for (( i = 1; i < number; i++ )); do
+        if (( i < 9 )); then
+            if (( i % 2 == 0 )); then
+                let "result = result + i + 1"
+            else
+                let "result = result - i - 1"
+            fi
+        fi
+    done
+    for (( i = 10; i <= number; i++ )); do
+        if (( i < 100 )); then
+            let "result = result - i/10 + i%10"
+        else
+            if (( i % 2 == 0 )); then
+                let "result = result - i/100 + (i%100)/10 - (i%100)%10"
+            else
+                let "result = result + i/100 - (i%100)/10 + (i%100)%10"
+            fi
+        fi
+    done
+
+
+    echo
+    echo result: $result
+    echo number: $number
+    echo
+fi
