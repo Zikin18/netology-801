9adf1251bfac8dc597308c64790369c1914912be
Zikin18 <zikin1988@mail.ru>
2023-10-04 23:06:42 +0300

    feat(calc): добавил калькулятор

--- test.sh
+++ test.sh
@@ -1,1 +1,17 @@
 echo "Здраствуйте, я ваш ученик"
+while true; do
+    read -p "Какое число первое? " n1
+    read -p "Какое число второе? " n2
+    PS3="Что за операция? "
+    select ans in сложение вычитание умножение деление; do
+        case $ans in
+            сложение) op='+' ; break ;;
+            вычитание) op='-' ; break ;;
+            умножение) op='*' ; break ;;
+            деление) op='/' ; break ;;
+            *) echo "Неверный ответ" ;;
+        esac
+    done
+    ans=$(echo "$n1 $op $n2" | bc -l)
+    printf "%s %s %s = %s\n\n" "$n1" "$op" "$n2" "$ans"
+done
